7d43b8335dba8e5d4d91f1b35ea071939d2ad141
Marcos <marcos_btv@live.com>
2020-08-05 20:54:15 -0300

    chore: api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://rest.coinapi.io',
   headers: {
-    'X-CoinAPI-Key': 'A4A783BE-AFDE-4988-9621-62B2729ED2E6'
+    'X-CoinAPI-Key': 'your key here'
   }
 });
 
