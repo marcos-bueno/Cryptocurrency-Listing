06d73778381f1fae01850a72d22b99b5d5f48a44
Marcos <marcos_btv@live.com>
2020-08-05 13:11:51 -0300

    chore: Commit inicial

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import './styles.css';
+import api from '../../services/api';
+import criptomoeda from '../../assets/criptomoeda.svg';
+
+interface Asset {
+  type_is_crypto: number;
+  asset_id: string;
+  name: string;
+  price_usd: number;
+  formattedPrice?: string;
+}
+
+// interface Rate {
+//   asset_id_base: string;
+//   rates: {
+//     asset_id_quote: string;
+//     rate: number;
+//   }
+// }
+
+const Home: React.FC = () => {
+  const [assets, setAssets] = useState<Asset[]>([]);
+  // const [rates, setRates] = useState<Rate[]>([]);
+
+  useEffect(() => {
+    api.get<Asset[]>('/v1/assets').then((response) => {
+      const assetsFormatted = response.data.map((asset) => {
+        return {
+          ...asset,
+          formattedPrice: new Intl.NumberFormat('en-IN',
+            { style: 'currency', currency: 'USD' })
+            .format(asset.price_usd),
+        };
+      });
+      setAssets(assetsFormatted);
+    });
+}, []);
+
+  const typeisCrypto = useMemo(() => {
+    return assets.filter(asset => {
+      return asset.type_is_crypto === 1;
+    });
+  }, [assets]);
+
+  return (
+    <>
+      <div className="container">
+        <div className="home">
+          <img 
+            className="mover" 
+            src={criptomoeda} 
+            height="136" 
+            alt="Criptomoeda" 
+          />
+          <h1 className="title">
+            Criptomoedas
+          </h1>
+        </div>
+        <table className="rwd-table">
+          <tbody>
+            <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Preço (USD)</th>
+            </tr>
+            {typeisCrypto.slice(0, 12).map((asset, index) => (
+            <tr key={asset.asset_id}>
+              <td>
+                {index + 1}
+              </td>
+              <td>
+                {asset.name}
+              </td>
+              <td>
+                {asset.formattedPrice}
+              </td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://rest.coinapi.io',
+  headers: {
+    'X-CoinAPI-Key': 'A4A783BE-AFDE-4988-9621-62B2729ED2E6'
+  }
+});
+
+export default api;
